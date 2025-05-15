a3caaf402acc1c6337c32393e80d596f42c57d5e
Dmitrii <Vdmitr11@outlook.com>
2025-05-15 23:29:13 +0300

    Добавил: - transform_file(), нужно разобраться с загрузкой в postgres.

--- download_dataset.py
+++ download_dataset.py
@@ -1,21 +1,19 @@
 import time
 import os
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from dotenv import load_dotenv
-
+from openpyxl import load_workbook
 
    
-URL = 'https://odoo.b4com.tech/web/login'
-    
-"""Импорт паролей для авторизации."""
-LOGIN = os.getenv('ODDO_LOGIN')
-PASSWORD = os.getenv('ODOO_PASSWORD')
-
-
-
 def load_dataset():
+    """Импорт паролей для авторизации."""
+    URL = 'https://odoo.b4com.tech/web/login'
+    LOGIN = os.getenv('ODDO_LOGIN')
+    PASSWORD = os.getenv('ODOO_PASSWORD')
+
     """Скачать dataset.XLSX"""
     driver = webdriver.Chrome()
     driver.maximize_window()
@@ -53,9 +51,39 @@ def load_dataset():
 
 def transform_file():
     """Сохрание файла в формате csv и пермещение в C:\Temp"""
-    pass
+    PATH_TO_XLSX = 'C:\\Users\\w3bfr\\Downloads'
+    PATH_TO_CSV = 'C:\\Temp\\raw_data.csv'  
+    files = os.listdir(PATH_TO_XLSX)
+    for file in files:
+        if file.endswith('.xlsx'):
+            book = load_workbook(os.path.join(PATH_TO_XLSX, file))
+            sheet = book.active
+            range_row = len(sheet['A'])
+            continue
+
+# Ошибка при загрузке файла в Postgres ругается на 3 строку.
+    with open(PATH_TO_CSV, 'a', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=',')
+        for i in range(1, range_row):
+            raw_data_col1 = f'{sheet['A' + str(i)].value}'
+            raw_data_col2 = f'{sheet['B' + str(i)].value}'
+            clear_col1 = raw_data_col1.strip()
+            clear_col2 = raw_data_col2.strip()           
+            if '[' not in clear_col1:
+                continue
+            elif ',' in clear_col1:
+               del_comma = clear_col1.replace(',', '')
+               writer.writerow([del_comma, clear_col2])
+            else:
+                writer.writerow([clear_col1, clear_col2])
+
+   
+            
+        
+    
 
 if __name__=="__main__":
     load_dotenv()
-    load_dataset()
+    #load_dataset()
+    transform_file()
     
