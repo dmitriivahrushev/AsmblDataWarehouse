5459eefe0fca4083c86fcfdf2849efc0051016a0
Dmitrii <Vdmitr11@outlook.com>
2025-05-17 16:16:47 +0300

    Добавил: - В функцию transform_file() запись логов в файл log.txt.

--- download_dataset.py
+++ download_dataset.py
@@ -1,6 +1,7 @@
 import time
 import os
 import csv
+from datetime import datetime as dt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -83,7 +84,9 @@ def transform_file():
             range_row = sheet.max_row
             continue
 
+    data_quality = {'All rows': range_row,'dirty rows': 0,'clear_rows': 0} # Проверка качества данных.
 
+    # Запись и чистка данных.
     with open(PATH_TO_CSV, 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=',')
         for i in range(1, range_row + 1):
@@ -92,18 +95,29 @@ def transform_file():
             clear_col1 = raw_data_col1.strip()
             clear_col2 = raw_data_col2.strip()           
             if '[' not in clear_col1:
+                data_quality['dirty rows'] += 1
                 continue
             elif ',' in clear_col1:
+               data_quality['clear_rows'] += 1
                del_comma = clear_col1.replace(',', '')
                writer.writerow([del_comma, clear_col2])
             else:
+                data_quality['clear_rows'] += 1
                 writer.writerow([clear_col1, clear_col2])
+    
+    # Запись логов в файл log.txt.
+    current_dir = os.path.join(os.curdir, 'log.txt')
+    with open (current_dir, 'a', newline='', encoding='utf-8') as file:
+        current_time = dt.now()
+        file.write(f'log [{current_time.strftime("%d/%m/%Y, %H:%M:%S")}]\n')
+        for key, values in data_quality.items():
+            file.write(f'{key}: {values}\n')
 
-     
+
 if __name__=="__main__":
     load_dotenv()
-    #del_files()
-    #load_dataset()
+    del_files()
+    load_dataset()
     transform_file()
 
     
